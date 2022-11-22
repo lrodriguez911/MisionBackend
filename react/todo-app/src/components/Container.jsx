322a2a41684b5cb08d67b2d66745628d362bf839
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-11-22 00:34:57 -0300

    add app-todo

--- react/todo-app/src/components/Container.jsx
+++ react/todo-app/src/components/Container.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import FormTodo from './FormTodo'
 import TaskList from './TaskList'
 
 const Container = () =>{
+    const [list, setList] = useState([])
+    
+    const addItem = addItem => {
+        setList([...list, addItem])
+    }
+
     return (
         <div>
-            <FormTodo />
-            <TaskList />
+            <FormTodo addItem={addItem}/>
+            <TaskList list={list} setList={setList}/>
         </div>
     )
 }
--- react/todo-app/src/components/FormTodo.jsx
+++ react/todo-app/src/components/FormTodo.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { useState } from "react";
 
+
+
 const FormTodo = props =>{
     const [description, setDescription] = useState("")
+    const {addItem} = props
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(description)
+        
+
+        addItem({
+            done: false,
+            id: (+new Date()).toString(),
+            description
+        })
+        setDescription("")
+    }
     return (
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <div className="todo-list">
                 <div className="input">
-                    <input type="text" className="text" value={description} />
-
+                    <input type="text" className="text" placeholder="Add task" value={description} onChange={e => setDescription(e.target.value)}/>
+                    <button className="button pink" disabled={description ? "": "disable"} >
+                    Add Task
+                    </button>
                 </div>
 
             </div>
