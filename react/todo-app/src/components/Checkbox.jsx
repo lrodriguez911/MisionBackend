401bf86d787afae1efdeaa7bf3ec50b4f946cc9c
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-11-24 01:05:40 -0300

    add new features to todo-app

--- react/todo-app/src/components/Checkbox.jsx
+++ react/todo-app/src/components/Checkbox.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {Fragment} from "react";
 
-const Checkbox = () =>{
-    return (
-        <div>Checkbox</div>
-    )
+const Checkbox = (props) =>{
+    const {onChange,
+    data: {id, description, done}} = props
+
+    return (<Fragment>
+        <label htmlFor="" className="todo new-item">
+            <input type="checkbox" className="todo_state" name={id} defaultChecked={done} onChange={onChange} />
+            <div className="todo_text">{description}</div>
+        </label>
+    </Fragment>)
 }
 
 export default Checkbox
