39c9958bd8916843be6a9d5b25690c5bcc927908
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-12-13 23:40:19 -0300

    work whit server.js

---
+++ Battleships-Multiplayer/server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const PORT = process.env.PORT || 3000;
+const socket = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+app.use(express.static(path.join(__dirname, "puclic")));
+
+server.listen(PORT, () => {
+  console.log(`the server is litening in port: ${PORT}`);
+});
+
+const connections = [null, null];
+
+io.on("connection", (socket) => {
+  let playerIndex = -1;
+  for (const i in connections) {
+    if (connections[i] === null) {
+      playerIndex = i;
+      break;
+    }
+  }
+
+  socket.emit("player-number", playerIndex);
+
+  console.log(`Player ${playerIndex} has been conected`);
+
+  if (playerIndex === -1) return;
+
+  connections[playerIndex] = false;
+
+  socket.broadcast.emit("player-connection", playerIndex);
+
+  socket.on("disconnect", () => {
+    console.log(`Player ${playerIndex} has been disconected`);
+    connections[playerIndex] = null;
+    socket.broadcast.emit("player-connection", playerIndex);
+  });
+
+  socket.on('player-ready', ()=>{
+    socket.broadcast.emit('enemy-ready', playerIndex);
+    connections[playerIndex] = true;
+  })
+
+  socket.on('check-players', ()=> {
+    const player = [];
+    for (const i in connections) {
+        connections[i] === null ? player.push({connected : false, ready: false}) : 
+        player.push({connected : true, ready: connections[i]});
+    }
+    socket.emit('check-players', players)
+
+    socket.on('fire', id => {
+        console.log(`Shoot from ${playerIndex}`, id);
+        socket.broadcast.emit('fire', id);
+    })
+    socket.on('fire-reply', square => {
+        console.log(square);
+        socket.broadcast.emit('fire-reply', square)
+    })
+
+    setTimeout(() => {
+        connections[playerIndex] = null;
+        socket.emit('timeout');
+        socket.disconnect()
+    }, 100000);
+  })
+});
